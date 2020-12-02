e2a17799a4673e93857474d2d1697335f21bdad2
Sxxov <sxxovnetwork@gmail.com>
2020-12-03 05:54:40 +0800

    Refactor

--- src/mixmix.ts
+++ src/mixmix.ts
@@ -1,19 +1,21 @@
-/* eslint-disable max-classes-per-file */
+type Class = (new (...args: any[]) => any);
+type ClassNameString = string;
 
-type ClassItem = (new (...args: any[]) => any);
+// ['length', 'prototype', 'name']
+// terser discards unused class names, so doing `class X {}`, would become `class {}`
+// in chrome, anonymous classes have the `name` property, but in node they don't
+const BASE_CLASS_PROPERTIES = Object.getOwnPropertyNames(class {}).concat('name');
+const BASE_CLASS_PROTOTYPE_PROPERTIES = ['constructor'];
 
-declare module 'mixmix';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function mixmix(
-	...classes: ClassItem[]
-): ClassItem {
-	const processedTargetClass = class MixMixed {
+export default function mixmix(
+	...classes: Class[]
+): Class {
+	class MixedClass {
 		/* \
 		|  | constructor will be executed if name of class is present as key in 'args',
 		|  |
 		|  | example...
-		|  | 	new mMixMixed({
+		|  | 	new MixMixed({
 		|  | 		A: [],
 		|  |		B: ['argument'],
 		|  |		IgApiClient: ['username', 'password'],
@@ -24,89 +26,100 @@ function mixmix(
 		|  |	new B('argument');
 		|  |	new IgApiClient('username', 'password');
 		\ */
-		constructor(args?: Record<string, any[]>) {
-			const classNames = classes
-				.map(
-					(sourceClass) => Object
-						.getOwnPropertyDescriptors(sourceClass)
-						.name
-						.value,
-				);
-			const argsKeys = ((): string[] => {
-				const keys = Object.keys(Object(args));
-
-				if (keys.length === 0) {
-					return classNames;
-				}
-
-				return keys;
-			})();
+		constructor(parametersMap: any[])
+		constructor(parametersMap: Record<ClassNameString, any[]>)
+		constructor(parametersMap: Record<ClassNameString, any[]> | any[] = null) {
+			const allClassNames = classes
+				.map((sourceClass) => sourceClass?.name);
+			const isIntantiatingAllWithOverrideValue = (
+				Array.isArray(parametersMap)
+				|| parametersMap == null
+			);
+			const parametersClassNames = isIntantiatingAllWithOverrideValue
+				? allClassNames
+				: Object.keys(parametersMap);
 
 			// use 'argsKeys' instead of 'className' to invoke in order of keys in 'args'
-			argsKeys.forEach(
-				(argsKey) => {
-					// only invoke constructor if 'className' is inside 'args' as a key
-					if (!classNames.includes(argsKey)) {
-						return;
-					}
+			for (let i = 0, l = parametersClassNames.length; i < l; ++i) {
+				const parametersClassName = parametersClassNames[i];
+				// if it's using override, then both arrays are the same, so skip the index scan
+				const indexOfParametersClassName = isIntantiatingAllWithOverrideValue
+					? i
+					: allClassNames.indexOf(parametersClassName);
 
-					// invoke!!!
-					Object
-						.defineProperties(
-							this,
-							Object
-								.getOwnPropertyDescriptors(
-									new (classes
-										.filter(
-											(sourceClass) => sourceClass.name === argsKey,
-										)[0])(),
+				// only invoke constructor if 'parametersClassName' is inside 'parametersMap' as a key
+				if (indexOfParametersClassName === -1) {
+					continue;
+				}
+
+				// invoke!!!
+				Object
+					.defineProperties(
+						this,
+						Object
+							.getOwnPropertyDescriptors(
+								new classes[indexOfParametersClassName](
+									...(
+										isIntantiatingAllWithOverrideValue
+											// will be array since it's override value
+											? parametersMap as any[]
+											: parametersMap[parametersClassName]
+									),
 								),
-						);
-				},
-			);
+							),
+					);
+			}
 		}
-	};
+	}
+
+	const mixedClassName = classes
+		.map((sourceClass) => sourceClass.name)
+		.join('');
+
+	Object.defineProperty(MixedClass, 'name', { value: mixedClassName });
 
 	// clone static items and items in prototype — items to be instantiated
 	// (except for constructor, it is handled above)
-	classes
-		.forEach(
-			(sourceClassItem) => {
-				// clone base class & prototype separately to be able to deal with constructor
-				defineClassProperties(processedTargetClass, sourceClassItem);
-				defineClassProperties(processedTargetClass.prototype, sourceClassItem.prototype);
-			},
+	for (let i = 0, l = classes.length; i < l; ++i) {
+		const sourceClass = classes[i];
+
+		// clone base class & prototype separately to be able to deal with constructor
+		defineProperties(
+			MixedClass,
+			sourceClass,
+			BASE_CLASS_PROPERTIES,
 		);
+		defineProperties(
+			MixedClass.prototype,
+			sourceClass.prototype,
+			BASE_CLASS_PROTOTYPE_PROPERTIES,
+		);
+	}
 
-	return processedTargetClass;
+	return MixedClass;
+}
 
-	function defineClassProperties(
-		targetClassItem: any,
-		sourceClassItem: any,
-	): typeof processedTargetClassItem {
-		const propertyNames = Object.getOwnPropertyNames(sourceClassItem);
-		const propertyDescriptors = Object.getOwnPropertyDescriptors(sourceClassItem);
-		const basePropertyNames = Object.getOwnPropertyNames(class X {}).concat(['constructor']);
+function defineProperties(
+	target: any,
+	source: any,
+	excludedNames: string[],
+): void {
+	const propertyDescriptors = Object.getOwnPropertyDescriptors(source);
+	const propertyNames = Object.keys(propertyDescriptors);
 
-		const processedTargetClassItem = targetClassItem;
+	for (let i = 0, l = propertyNames.length; i < l; ++i) {
+		const propertyName = propertyNames[i];
 
-		propertyNames
-			.forEach(
-				(propertyName) => {
-					// ensure it is a unique property (not 'length', 'prototype', 'name', 'constructor')
-					if (basePropertyNames.includes(propertyName)) {
-						return;
-					}
+		// ensure it is a unique property (not 'length', 'prototype', 'name', 'constructor')
+		if (excludedNames.indexOf(propertyName) !== -1) {
+			continue;
+		}
 
-					// define 'propertyName': 'propertyDescriptors[propertyName]'
-					Object.defineProperty(
-						processedTargetClassItem,
-						propertyName,
-						propertyDescriptors[propertyName],
-					);
-				},
-			);
-
-		return processedTargetClassItem;
+		// define 'propertyName': 'propertyDescriptors[propertyName]'
+		Object.defineProperty(
+			target,
+			propertyName,
+			propertyDescriptors[propertyName],
+		);
 	}
 }
